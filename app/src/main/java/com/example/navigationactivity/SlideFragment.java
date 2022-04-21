0fee961a929a3a6397e7f71c8138f2ca02fcf751
pkumaresangithub <pkumaresanin@gmail.com>
2022-04-21 20:24:24 +0530

    first commit

---
+++ app/src/main/java/com/example/navigationactivity/SlideFragment.java
@@ -0,0 +1,19 @@
+package com.example.navigationactivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SlideFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_slide, container, false);
+    }
+}
